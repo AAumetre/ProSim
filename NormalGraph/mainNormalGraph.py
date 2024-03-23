37aeafaa9d6a13bf0dd26da4f68b3ebc61d195f0
Antonin Aumètre <antonin.aumetre@gmail.com>
2024-03-23 17:39:10 +0100

    remove mixed law computation

--- NormalGraph/mainNormalGraph.py
+++ NormalGraph/mainNormalGraph.py
@@ -5,40 +5,17 @@ import time
 
 import matplotlib.pyplot as mp
 from typing import *
+
+import numpy
 import scipy as scipy
 from networkx import DiGraph
 
 from NormalGraph.GraphMonteCarlo import MonteCarlo, NodeType
-from NormalMixedLaw import NormalMixedLaw
 from Task import Task
 import networkx as nx
 
 Function1D = Callable[[float], float]
 DiscreteFunction1D = Tuple[List[float], List[float]]
-
-
-def build_mixed_law(graph_: DiGraph, start_: NodeType, end_: NodeType, dx_: float) -> DiscreteFunction1D:
-    """ Uses a NetworkX graph to build a mixed probability distribution law. """
-    laws = []
-    # explore the graph and build a law on each path
-    paths = nx.all_simple_paths(graph_, start_, end_)
-    for path in paths:
-        mixed = NormalMixedLaw(dx_)  # initialise the mixed law
-        law_p = 1.0
-        for u, v in itertools.pairwise(path):
-            p = graph_[u][v]["weight"]
-            m, s = v.normal_
-            mixed.add_normal_densities([(p, m, s)])
-            law_p *= p
-        laws.append((law_p, mixed))
-    # sum all the laws together
-    mixed_x, mixed_y = laws[0][1].get_sampling()  # starts at zero
-    mixed_y = [y * laws[0][0] for y in mixed_y]
-    for law in laws[1:]:  # TODO: can be optimized
-        other_x, other_y = law[1].get_sampling()
-        other_y = [y * law[0] for y in other_y]
-        mixed_x, mixed_y = sum_discrete_functions((mixed_x, mixed_y), (other_x, other_y), dx_)
-    return mixed_x, mixed_y
 
 
 def sum_discrete_functions(f1_: DiscreteFunction1D, f2_: DiscreteFunction1D, dx_: float) -> DiscreteFunction1D:
@@ -85,7 +62,18 @@ def load_graph_json(path_: str):
     with open(path_) as f:
         data = json.load(f)
         for t in data["tasks"]:
-            nodes[t["name"]] = Task(t["name"], (t["normal"][0], t["normal"][1]), t["layer"])
+            # two possibilities to evaluate the distribution: either "three-points" or "normal"
+            # in both cases, we need to evaluation the expectation and variance
+            if "normal" in t:
+                expectation = t["normal"][0]
+                variance = t["normal"][1]
+            elif "three-points" in t:
+                tp = sorted(t["three-points"])
+                expectation = (tp[0] + 4*tp[1] + tp[2])/6.0
+                variance = max((tp[2] - tp[0]) / 6.0, 1e-6)
+            else:
+                print(f"Invalid line in file, cannot process task {t}")
+            nodes[t["name"]] = Task(t["name"], (expectation, variance), t["layer"])
         starting_node = nodes[data["starting_task"]]
         ending_node = nodes[data["ending_task"]]
         # read edges definition
@@ -111,7 +99,7 @@ def main():
     mp.style.use("bmh")
 
     # Define a graph describing the process, starting with the tasks
-    start_node, end_node, g = load_graph_json("Examples/RiskAlternative.json")
+    start_node, end_node, g = load_graph_json("Examples/SimpleRiskThreePoints.json")
 
     # do a Monte-Carlo simulation
     solver = MonteCarlo(g, start_node)
@@ -119,31 +107,24 @@ def main():
     mc_sample = solver.compute_sample(n_mc_samples, True)
 
     # draw the graph
-    options = {"node_size": 3000, "font_color": "white", "arrowsize": 20}
+    options = {"node_size": 3000, "font_color": "black", "arrowsize": 20}
     positioning = nx.multipartite_layout(g, subset_key="layer")
     nx.draw_networkx(g, pos=positioning, with_labels=True, font_weight='bold', **options)
     edge_labels = nx.get_edge_attributes(g, "weight")
     nx.draw_networkx_edge_labels(g, positioning, edge_labels)  # shown later
 
-    # browse the graph and build the mixed probability law
     dx = 0.01
-    mixed = build_mixed_law(g, start_node, end_node, dx)
-    mass_function = scipy.integrate.cumulative_trapezoid(mixed[1], mixed[0])
 
     # plot density and mass functions
     fig, ax1 = mp.subplots()
     color = 'tab:green'
     ax1.set_xlabel("Cost [h]")
     ax1.set_ylabel("Density function", color=color)
-    # scale up the mixed law for comparison with Monte Carlo hist
     n_bins, mc_intervals, _2 = ax1.hist(mc_sample, bins=n_mc_samples//20)
-    mixed = mixed[0], [max(n_bins) * y for y in mixed[1]]
-    ax1.plot(mixed[0], mixed[1], color=color)
     ax1.tick_params(axis='y', labelcolor=color)
     ax2 = ax1.twinx()
     color = 'tab:red'
     ax2.set_ylabel("Mass function", color=color)
-    ax2.plot(mixed[0][:-1], mass_function, color=color)
     mass_carlo = scipy.integrate.cumulative_trapezoid(n_bins, mc_intervals[:-1])
     mass_carlo = [v/max(mass_carlo) for v in mass_carlo]  # normalize mass function
     ax2.plot(mc_intervals[:-2], mass_carlo, "--")
@@ -154,10 +135,9 @@ def main():
     mp.show()
 
     # print out some metadata
-    integral = sum(mixed[1])*dx
-    expected = compute_expectation(mixed) / integral
+    expected = numpy.mean(mc_sample)
     print(f"Expectation: {expected:.2f} hours.")
-    variance = compute_variance(mixed, expected) / integral
+    variance = numpy.var(mc_sample)
     print(f"Variance: {variance:.2f}.")
 
 
